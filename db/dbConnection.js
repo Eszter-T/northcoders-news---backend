d89b09113c49fc43e7e51d93343cda1e2c852a89
Eszter Tarjanyi <eszter.tarjanyi94@gmail.com>
2021-03-19 12:52:40 +0000

    make changes to host app

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,4 +1,17 @@
+const ENV = process.env.NODE_ENV || 'development';
 const dbConnection = require('knex');
-const dbConfig = require('../knexfile');
+
+const dbConfig =
+  ENV === 'production'
+    ? {
+        client: 'pg',
+        connection: {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
+      }
+    : require('../knexfile');
 
 module.exports = { dbConnection: dbConnection(dbConfig) }
