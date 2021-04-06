e9d18254a0f81f14078e138a134f604abf37c4e9
Eszter Tarjanyi <eszter.tarjanyi94@gmail.com>
2021-04-06 14:34:30 +0100

    update with cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { handlePsqlErrors, handleCustomErrors, handleServerErrors } = require('./errors/index');
+const cors = require('cors');
 const app = express();
 const apiRouter = require('./routes/apiRouter');
 
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 
